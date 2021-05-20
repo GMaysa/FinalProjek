a7b36676da7677517e188d42d7f65f4c17302f9a
Yessyow <maysaputra0205@gmail.com>
2021-05-20 14:48:12 +0800

    DONE

--- member.c
+++ member.c
@@ -32,7 +32,52 @@ int insert(){
     puts("BERHASIL...");
     return 0;
 }
-int update(){}
+int update(){
+    FILE *ptr, *w;
+    char ch, nama[100];
+    ptr = fopen("DB/dbMember.txt", "r");
+
+    int i = 0;
+    while (ch != EOF)
+    {
+        fscanf(ptr, "%200[^;]%*c %200[^;]%*c %200[^;]%*c", mem[i].nama, mem[i].noTelp, mem[i].Alamat);
+        ch = fgetc(ptr);
+        i++;
+    }
+    printf("Masukkan nama member: ");
+    fflush(stdin);
+    gets(nama);
+    for (int j = 0; j < i; j++)
+    {
+        if (strcmp(mem[j].nama, nama) == 0)
+        {
+            puts("Masukkan data dengan benar...");
+            printf("Nama: ");
+            fflush(stdin);
+            gets(mem[j].nama);
+            printf("No Telp: ");
+            fflush(stdin);
+            gets(mem[j].noTelp);
+            printf("Alamat: ");
+            fflush(stdin);
+            gets(mem[j].Alamat);
+        }
+        system("cls");
+    }
+    system("cls");
+    w = fopen("DB/dbMember.txt", "w");
+    for (int j = 0; j < i; j++)
+    {
+        if(j == 0){
+            fprintf(w, "%s; %s; %s;", mem[j].nama, mem[j].noTelp, mem[j].Alamat);
+        }else{
+            fprintf(w, " %s; %s; %s;", mem[j].nama, mem[j].noTelp, mem[j].Alamat);
+        }
+        printf("Nama: %s\nNo Telp: %s\nAlamat: %s\n\n", mem[j].nama, mem[j].noTelp, mem[j].Alamat);
+    }
+    fclose(w);
+    fclose(ptr);
+}
 
 int display(){
     FILE *ptr;
@@ -47,14 +92,11 @@ int display(){
     int i = 0;
     while(ch != EOF){
         printf("%d\n", i);
-        fscanf(ptr, "%[^;]%*c %200[^;]%*c %200[^;]%*c", mem[i].nama, mem[i].noTelp, mem[i].Alamat);
+        fscanf(ptr, "%200[^;]%*c %200[^;]%*c %200[^;]%*c", mem[i].nama, mem[i].noTelp, mem[i].Alamat);
         printf("Nama: %s\nNo Telp: %s\nAlamat: %s\n\n", mem[i].nama, mem[i].noTelp, mem[i].Alamat);
         i++;
         ch = fgetc(ptr);
     }
-    // for(int j = 0; j < i; j++){
-    //     if(j == 0);
-    // }
     fclose(ptr);
 }
 
